89bcd89aa4d97a8a4bf478e453932e9795451405
Ian Auld <imauld@gmail.com>
2018-07-20 13:59:58 -0700

    Doing some things

--- eventlib/event.py
+++ eventlib/event.py
@@ -1,1 +1,26 @@
 """Event module."""
+
+
+class Event(object):
+    def __init__(self, *args, **kwargs):
+        """
+        channel: raw.SUBSCRIBE[1],
+        type: parsed.type,
+        messageID: parsed.messageID,
+        avatarURL: parsed.avatarURL,
+        username: parsed.username,
+        userPK: parsed.userPK,
+        text: parsed.text,
+        timestamp : parsed.timestamp,
+        """
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __iter__(self):
+        for key, value in self.__dict__.items():
+            if not key.startswith('_'):
+                yield key, value
+
+    def __repr__(self):
+        return ";".join("{}::{}".format(k, v) for k, v in self)
--- eventlib/stream.py
+++ eventlib/stream.py
@@ -1,1 +1,8 @@
 """Stream module"""
+
+
+class Stream(object):
+    def __init__(self, name, stream, serialization):
+        self.name = name
+        self.stream = stream
+        self.serialization = serialization
